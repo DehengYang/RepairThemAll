31961212d76ea8d1c2d5193d3edfa4d4367658e3
DehengYang <1660542154@qq.com>
2020-02-13 20:11:00 -0800

    annotate config.py & checkout.py via debugging in vscode

--- script/checkout.py
+++ script/checkout.py
@@ -9,10 +9,21 @@ parser.add_argument("--benchmark", "-b", required=True, default="Defects4J",
 parser.add_argument("--id", "-i", required=True, help="The bug id")
 parser.add_argument("--working_directory", "-w", required=True, help="The working directory")
 
+# e.g., 
+# /usr/bin/python /mnt/repairthemall/script/checkout.py  
+# --benchmark Bears --id webfirmframework-wff-453188520-453188718 --working_directory /mnt/workingDir
 if __name__ == "__main__":
+    # receive -b, -i, -w parameters
     args = parser.parse_args()
+
+    # get whole benchmark
     args.benchmark = get_benchmark(args.benchmark)
+
+    # get single bug
     bug = args.benchmark.get_bug(args.id)
+
+    # identify workdir
     bug_path = os.path.join(args.working_directory,
                             "%s_%s_%s" % (bug.benchmark.name, bug.project, bug.bug_id))
+    # checkout bug and cp to workdir
     bug.checkout(bug_path)
--- script/config.py
+++ script/config.py
@@ -2,22 +2,38 @@ import os
 from os.path import expanduser
 from core.Support import getGridTime
 
+# root dir: /mnt/repairthemall/script/..
 REPAIR_ROOT = os.path.join(os.path.dirname(__file__), '..')
+
+# data path: /mnt/repairthemall/script/../data
 DATA_PATH = os.path.join(REPAIR_ROOT, "data")
+
+# repair tool dir: repair_tools
 REPAIR_TOOL_FOLDER = os.path.join(REPAIR_ROOT, "repair_tools")
-WORKING_DIRECTORY = os.path.join("/tmp/")
+
+# work dir: /mnt/workingDir/
+WORKING_DIRECTORY = os.path.join("/mnt/workingDir/")
+
+# result dir: results/ (seems it will be created later)
 OUTPUT_PATH = os.path.join(REPAIR_ROOT, "results/")
 
+# z3 for nopol: libs/z3/build
 Z3_PATH = os.path.join(REPAIR_ROOT, "libs", "z3", "build")
 
-JAVA7_HOME = expanduser("/usr/lib/jvm/java-1.7.0-openjdk-amd64/bin/")
-JAVA8_HOME = expanduser("/usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/")
+# jdk 7 and 8
+# JAVA7_HOME = expanduser("/usr/lib/jvm/java-1.7.0-openjdk-amd64/bin/")
+# JAVA8_HOME = expanduser("/usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/")
+JAVA7_HOME = expanduser("/home/apr/env/jdk1.7.0_80/bin/")
+JAVA8_HOME = expanduser("/home/apr/env/jdk1.8.0_202/bin/")
 JAVA_ARGS = "-Xmx4g -Xms1g"
 
+# thread number
 LOCAL_THREAD = 1
 GRID5K_MAX_NODE = 50
+
 ##In minutes
 TOOL_TIMEOUT = "120"
+
 #Format: HH:MM ## the fuction getGridTime calculates the timeout of the grid taking into account an overhead (expressed as percentage)
 GRID5K_TIME_OUT = getGridTime(TOOL_TIMEOUT, overhead=0.33)
 
